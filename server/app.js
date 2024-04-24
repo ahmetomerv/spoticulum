d0ed4ace3862428c1c1ef3684d19b469334ca11d
Ahmet Ömer <ahmetomerv@gmail.com>
2024-04-25 00:26:08 +0200

    project structure update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
-import AuthRoutes from './routes/authRoutes.js';
+import AuthRoutes from './src/routes/authRoutes.js';
 
 const PORT = process.env.PORT || 8888;
 const app = express();
