aa3455303566a99bef23116a7a654afa8783db7e
Ahmet Ömer <ahmetomerv@gmail.com>
2024-05-07 01:35:52 +0200

    default profile photo fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -103,7 +103,7 @@ class Home extends React.Component {
     let profileUrl = 'default-profile-icon-16.jpg';
     const exampleCollectionUrl = 'example-collection.png';
 
-    if (user && user.images) {
+    if (user && user.images && user.images) {
       profileUrl = user.images[0].url;
     }
 
